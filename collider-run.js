66faa7f951c3e5d8e4961700eb1f57ed0eaa8bfa
Simon Sinclair <simon@sinclair>
2016-03-10 10:47:51 +0000

    Update collider-run

--- collider-run.js
+++ collider-run.js
@@ -5,8 +5,22 @@
 var pkg = require('./package.json')
 var cli = require('commander');
 
+var fs    = require('fs');
+var path  = require('path');
+var spawn = require('child_process').spawn;
+
 cli
   .version( pkg.version )
   .parse( process.argv );
 
-// console.log( cli );
+var cwd = process.cwd();
+var colliderFile = path.join( cwd, '.collider');
+
+// Check if a Collider-file exists in the current working directory before running Gulp.
+fs.access( colliderFile, fs.F_OK, function(error) {
+  if(error) {
+    console.log('A Collider-file could not be found in this directory.');
+  } else {
+    spawn('./node_modules/.bin/gulp', ['default'], { stdio: 'inherit' });
+  }
+});

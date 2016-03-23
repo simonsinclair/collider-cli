0fa905726bc545f6809aac20c7a3c8579dadb647
Simon Sinclair <simon@sinclair>
2016-03-23 17:01:22 +0000

    Use creatError and logErrorExit to handle errors

--- bin/collider-run.js
+++ bin/collider-run.js
@@ -5,6 +5,9 @@
 var pkg = require('../package.json')
 var cli = require('commander');
 
+var createError  = require('../lib/createError');
+var logErrorExit = require('../lib/logErrorExit');
+
 var fs    = require('fs');
 var path  = require('path');
 var spawn = require('child_process').spawn;
@@ -17,9 +20,10 @@ var cwd = process.cwd();
 var colliderFile = path.join( cwd, 'project', '.collider');
 
 // Check if a Collider-file exists in the current working directory before running Gulp.
-fs.access( colliderFile, fs.F_OK, function(error) {
-  if(error) {
-    console.log('A Collider-file could not be found in this directory.');
+fs.access( colliderFile, fs.F_OK, function(err) {
+  if(err) {
+    var err = createError("a Collider-file couldn't be found. Is this a Collider project?");
+    logErrorExit( err, true );
   } else {
     spawn('./node_modules/.bin/gulp', ['default'], { stdio: 'inherit' });
   }

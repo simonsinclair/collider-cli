8fde4d20b27c1e01f54644dcfc7fd5bca8bc99a6
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-04-21 17:59:26 +0100

    Refactor to use 'yargs' instead of 'commander'

--- bin/collider.js
+++ bin/collider.js
@@ -3,13 +3,23 @@
 'use strict';
 
 var pkg            = require('../package.json')
-var cli            = require('commander');
+var cli            = require('yargs');
 var updateNotifier = require('update-notifier');
 
 updateNotifier({ pkg }).notify();
 
 cli
-  .version( pkg.version )
-  .command( 'run', 'runs an existing project in the current directory', { isDefault: true } )
-  .command( 'new <project-name>', 'creates a new project in the current directory' )
-  .parse( process.argv );
+  .strict()
+  .version()
+  .usage('collider [--version] [--help] <command> [<args>]')
+  .command('run', 'Run an existing project in the current directory', require('../lib/commands/run'))
+  .command('new <name>', 'Create a new project in the current directory', require('../lib/commands/new'))
+  .help()
+  .argv;
+
+var commands = cli.argv._;
+
+// If no commands are given, then show help.
+if(commands.length === 0) {
+  cli.showHelp();
+}

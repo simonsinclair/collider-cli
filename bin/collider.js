294b2c1d7c7384c73573f710aa8e26e8591a0d39
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-04-22 16:54:08 +0100

    Add matter/clone,remove generate and status commands

--- bin/collider.js
+++ bin/collider.js
@@ -14,6 +14,13 @@ yargs
   .usage('collider [--version] [--help] <command> [<args>]')
   .command('run', 'Run an existing project in the current directory', require('../lib/commands/run'))
   .command('new <name>', 'Create a new project in the current directory', require('../lib/commands/new'))
+  .command('matter', 'Manage Matter libraries in the current project', function (yargs) {
+    return yargs
+      .command('clone <url> [locale]', 'Clone a Matter library into the current project', require('../lib/commands/matter/clone'))
+      .command('remove <locale>', 'Remove a Matter library from the current project', require('../lib/commands/matter/remove'));
+  })
+  .command('generate <type> <paths..>', 'Generate skeleton Matter within the current project', require('../lib/commands/generate'))
+  .command('status', 'Show information about the current project', require('../lib/commands/status'))
   .help()
   .argv;
 

1a80c58a5dfab0181003b74bd4ad54bd111dcd20
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-05-11 15:39:32 +0100

    Refactor command call mechanism

--- bin/collider.js
+++ bin/collider.js
@@ -6,11 +6,7 @@ var createError  = require('../lib/createError');
 var logErrorExit = require('../lib/logErrorExit');
 var pkg = require('../package.json');
 
-var cmdRun      = require('../lib/commands/run');
-var cmdNew      = require('../lib/commands/new');
-var cmdMatter   = require('../lib/commands/matter');
-var cmdGenerate = require('../lib/commands/generate');
-var cmdHelp     = require('../lib/commands/help');
+var cmds = require('../lib/commands');
 
 var docopt = require('docopt').docopt;
 var updateNotifier = require('update-notifier');
@@ -42,22 +38,9 @@ var args = docopt(doc, {
 var cmd  = args['<command>'];
 var argv = [cmd].concat(args['<args>']);
 
-switch (cmd) {
-  case 'run':
-    cmdRun(argv);
-    break;
-  case 'new':
-    cmdNew(argv);
-    break;
-  case 'matter':
-    cmdMatter(argv);
-    break;
-  case 'generate':
-    cmdGenerate(argv);
-    break;
-  case 'help':
-    cmdHelp(argv);
-    break;
-  default:
-    console.error(`collider: "${cmd}" is not a collider command. See 'collider --help'.`);
+if (typeof cmds[cmd] !== 'undefined') {
+  cmds[cmd](argv);
+} else {
+  var err = createError(`"${cmd}" is not a collider command. See 'collider --help'.`);
+  logErrorExit(err, true);
 }

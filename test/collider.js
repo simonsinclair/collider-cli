b68867c966562f11bcc2ecbc8b79cd00d4c490a1
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-05-20 17:01:26 +0100

    Write `collider` test cases

--- test/collider.js
+++ test/collider.js
@@ -3,6 +3,8 @@
 
 'use strict';
 
+var pkg = require('../package.json');
+
 var fs = require('fs');
 var os = require('os');
 var path = require('path');
@@ -56,21 +58,48 @@ before('prepare', function () {
   TempDir.prepare();
 });
 
-describe('collider-cli', function () {
+describe('collider', function () {
 
-  it('should return a usage string', function () {
+  it('should show a usage format when passed no args.', function () {
+    var result = TempDir.collider();
 
+    // TO DO:
+    // Look into why exit code (1) is being
+    // routed to STDOUT rather than STDERR.
+    var text = result.stdout.toString();
+
+    expect(text).to.contain('usage:');
+    expect(text).to.not.contain('options:');
+  });
+
+  it('should show current version when passed "--version"', function () {
+    var result = TempDir.collider(['--version']);
+    var text   = result.stdout.toString();
+
+    expect(text).to.contain(pkg.version);
+  });
+
+  it('should show help information when passed "--help"', function () {
+    var result = TempDir.collider(['--help']);
+    var text   = result.stdout.toString();
+
+    expect(text).to.contain('options:');
+    expect(text).to.contain('commands:');
+  });
+
+  it('should exit with an error when passed an undefined command', function () {
+    var result = TempDir.collider(['undefined']);
+    var text   = result.stderr.toString();
+
+    expect(result.status).to.not.equal(0);
+    expect(text).to.contain('Error:');
   });
 
   describe('run', function () {
 
-    it('should run an existing project', function () {
+    // it('should run an existing project', function () {
 
-    });
-
-    it('should throw an error when run from a non-existent project', function () {
-
-    });
+    // });
   });
 });
 

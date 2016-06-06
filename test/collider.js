6c4585fd7cf0462cb7fe8663a1818e134560e631
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-06-06 16:32:53 +0100

    Write `new` tests

--- test/collider.js
+++ test/collider.js
@@ -6,6 +6,7 @@
 var pkg = require('../package.json');
 var tempDir = require('./lib/TempDir');
 
+var fs = require('fs');
 var os = require('os');
 var path = require('path');
 
@@ -13,7 +14,7 @@ var expect = require('chai').expect;
 var nock = require('nock');
 var uuid = require('node-uuid');
 
-// Throw "NetConnectNotAllowedError" on any requests to 'un-Nocked' hosts.
+// Disable all 'un-Nocked' requests.
 nock.disableNetConnect();
 
 // Set a unique temporary location.
@@ -68,19 +69,58 @@ describe('collider', function () {
 
   describe('new', function () {
 
-    it('should create a new project', function () {
+    // To do:
+    // Find a better way to run (mock) this test.
+    it('should create a new project', function (cb) {
+      this.timeout(10000);
 
+      var filePath = __dirname + '/assets/latest.tar.gz';
+      var contentLength = fs.statSync(filePath)['size'];
+
+      var scope = nock('http://getcollider.com')
+        .log(console.log)
+        .get('/latest.tar.gz')
+        .replyWithFile(200, filePath, {
+          'Content-Length': contentLength
+        });
+
+      tempDir.runCmd('new', ['test-project'], tempDir.location, function () {
+        expect(tempDir.exists('test-project/project/.collider')).to.be.true;
+        cb();
+      });
     });
 
     // Collider-file reference.
     // {"name":"","createdTime":0,"author":"","matterLibs":[]}
 
     it('should populate the Collider-file', function () {
-
+      var project = tempDir.readJson('test-project/project/.collider');
+      expect(project.name).to.deep.equal('test-project');
+      expect(project.createdTime).to.be.above(0);
+      expect(project.author).to.equal('unknown');
+      expect(project.matterLibs).to.be.empty;
     });
 
-    it('should populate the Collider-file with the attributed author', function () {
+    // To do:
+    // Find a better way to run (mock) this test.
+    it('should populate the Collider-file with the attributed author', function (cb) {
+      this.timeout(10000);
 
+      var filePath = __dirname + '/assets/latest.tar.gz';
+      var contentLength = fs.statSync(filePath)['size'];
+
+      var scope = nock('http://getcollider.com')
+        .log(console.log)
+        .get('/latest.tar.gz')
+        .replyWithFile(200, filePath, {
+          'Content-Length': contentLength
+        });
+
+      tempDir.runCmd('new', ['--author', 'Test Author', 'test-project-author'], tempDir.location, function () {
+        var author = tempDir.readJson('test-project-author/project/.collider').author;
+        expect(author).to.deep.equal('Test Author');
+        cb();
+      });
     });
   });
 
@@ -118,5 +158,6 @@ afterEach(function () {
 });
 
 after('clean', function () {
+  this.timeout(10000);
   tempDir.clean();
 });
